2b4a8e2802c3b884c486fe46a24da2f0f85ecbfb
grootforce2 <grootforce2@users.noreply.github.com>
2025-12-19 02:48:29 +0800

    Premium MVP v1: Physio OS shell (clients/calendar/exercises/programs/intake/progress/reports)

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,7 @@
-﻿import 'package:flutter/material.dart';
-import 'premium_dashboard.dart';
-import 'app.dart';
+﻿import 'package:flutter/widgets.dart';
+import 'premium/premium_physio_os_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const _PremiumApp());
+  runApp(const PremiumPhysioOSApp());
 }
-
-
-class _PremiumApp extends StatelessWidget {
-  const _PremiumApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Simon Physio',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: const Color(0xFF3B82F6),
-        scaffoldBackgroundColor: const Color(0xFFF6F7FB),
-      ),
-      home: const PremiumDashboard(),
-    );
-  }
-}
-
